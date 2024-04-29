eea181fcbf718af0063b2bc4848cb717cfba2225
Matheus Honorato <matheuswebmw@gmail.com>
2024-04-29 00:01:05 -0300

    baixa arquivo, lê o csv  e exibe a  temperatura máxima

--- exercicio-2/main.py
+++ exercicio-2/main.py
@@ -1,5 +1,5 @@
 import requests
-import pandas
+import pandas as pd
 import os
 from bs4 import BeautifulSoup
 
@@ -15,6 +15,8 @@ def main():
     
     url_download = []
     pasta_destino = "downloads/"
+
+    existe_arquivo = False #Verificar se o arquivo foi baixado
     if (response.ok):
 
         html = response.content
@@ -30,13 +32,27 @@ def main():
                         link = coluna[0].find("a")
                         if link:
                             url_download.append(url+link.get("href"))
-        
+        os.makedirs(pasta_destino, exist_ok=True) ##Criando a pasta downloads
+        caminho_arquivo = os.path.join(pasta_destino,url_download[0].split("/")[-1])
+        arquivo = requests.get(url_download[0])
+        if arquivo.ok:
+            with open(caminho_arquivo, mode="wb") as f:
+                f.write(arquivo.content)
+                existe_arquivo = True
         
     else:
         print("Solicitação falhou. Código do status:", response.status_code)
 
 
-    print(url_download[0])
+    if(existe_arquivo):
+        df = pd.read_csv(caminho_arquivo)
+        temp_max_index = df['HourlyDryBulbTemperature'].idxmax()
+        temp_max = df['HourlyDryBulbTemperature'][temp_max_index]
+        print(temp_max)
+        os.remove(caminho_arquivo)
+    else:
+        "Arquivo não existe"
+
 
 
     
